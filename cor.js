38d306a343266e7f872a43f82d811ecc65a8bf50
Ceci-rosa <sanaceciliarosade@gmail.com>
2026-06-10 08:30:43 -0300

    Add files via upload

--- cor.js
+++ cor.js
@@ -11,3 +11,34 @@ switcher.addEventListener('click', function () {
     }
 
 }); 
+
+function getProjects(){
+    const urlGithub = 'https://api.github.com/users/Ceci-rosa/repos'
+    var loadingElement = document.getElementById('loading')
+    fetch(urlGithub, {
+        method: 'GET'
+    })
+    .then((response) => response.json())
+    .then((response) => {
+        loadingElement.style.display = 'none'
+        showProjects(response)
+    })
+    .catch((e) => {
+        console.log(e)
+    })
+}
+
+function showProjects(data){
+    var listElement = document.getElementById('meus-projetos-lista')
+    for(let i = 0; i < data.length; i++){
+        let a = document.createElement('a')
+        a.href = data[i]['clone-url']
+        a.target = '_blank'
+        a.title = data[i]['description']
+        let linkText = document.createTextNode(data[i]['name'])
+        a.appendChild(linkText)
+        listElement.appendChild(a)
+    }
+}
+
+getProjects()
